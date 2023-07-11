cef1140adccb73c6c3d917b221bc2c1ab70d0da6
Jordan H <jordan@hrvy.uk>
2023-07-11 22:36:27 +0100

    ghactions replaces version in CONST

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 import subprocess
-
+import io, re
 
 def get_last_version() -> str:
     """Return the version number of the last release."""
@@ -25,6 +25,17 @@ def bump_patch_number(version_number: str) -> str:
     return f"{major}.{minor}.{int(patch) + 1}"
 
 
+def update_file_version(new_version):
+    """Updates the VERSION variable in const.py"""
+    with open("pyroostermoney/const.py", "r") as file:
+        filedata = file.read()
+
+    # replace str
+    filedata = re.sub('VERSION=".*?"', f'VERSION="{new_version}"', filedata)
+
+    with open("pyroostermoney/const.py", "w") as file:
+        file.write(filedata)
+
 def create_new_patch_release():
     """Create a new patch release on GitHub."""
     try:
@@ -37,6 +48,26 @@ def create_new_patch_release():
             raise
     else:
         new_version_number = bump_patch_number(last_version_number)
+
+    update_file_version(new_version_number)
+
+    subprocess.run(
+        ["git", "config", "user.name", "github-actions"]
+    )
+
+    subprocess.run(
+        ["git", "config", "user.email", "github-actions@github.com"]
+    )
+
+    subprocess.run(
+        ["git", "commit", "-m", "\"(auto) bump version for release\""],
+        check=True
+    )
+
+    subprocess.run(
+        ["git", "push", "-u", "origin", "HEAD"],
+        check=True
+    )
 
     subprocess.run(
         ["gh", "release", "create", "--generate-notes", new_version_number],
