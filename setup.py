6f8af7db9ed31f0b51b9c8a6e739ad5271ad3051
Jordan H <jordan@hrvy.uk>
2023-08-23 20:19:46 +0100

    0.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="pyroostermoney",
-      version="0.3.3",
+      version="0.3.4",
       description="A RoosterMoney integration for Python.",
       url="https://github.com/pantherale0/pyroostermoney",
       author="pantherale0",
